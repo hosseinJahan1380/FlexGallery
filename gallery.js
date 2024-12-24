2080276eb35e7a6e0412b02f14437b9a02dd0a4a
hossein <hosseinjahanbanifar@gmail.com>
2024-12-24 20:40:56 +0330

    Adjusted image box layout to maintain consistent sizes and alignment

--- gallery.js
+++ gallery.js
@@ -1,13 +1,20 @@
 
 const filterItems = document.querySelectorAll('.filters button')
-const images = document.querySelectorAll('.images img')
+const images_box = document.querySelectorAll('.box-image')
+const all_filter = document.querySelector('.filters #all')
+all_filter.classList.add('active')
 
 filterItems.forEach( item => {
     item.addEventListener('click' ,()=>{
+        filterItems.forEach(item => item.classList.remove('active'))
+        item.classList.add('active')
+
         const filter = item.dataset.filter
-    
-        images.forEach(image=>{
-           (filter ==='all' || image.dataset.category === filter) ? image.classList.remove('hidden') : image.classList.add('hidden')
+        images_box.forEach(box=>{
+            const image = box.querySelector('img')
+            if(image){
+                (filter ==='all' || image.dataset.category === filter) ? box.classList.remove('hidden') : box.classList.add('hidden')
+            }
         })
     })
 })
